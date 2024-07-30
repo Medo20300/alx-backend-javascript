d79a8071cd3fad150bca08bb7655a2f20b1294ae
Medo20300 <medhatconcrete@gmail.com>
2024-07-30 12:23:06 +0300

    IDo

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,4 +1,10 @@
 export default function setFromArray(arr) {
-  return new Set(arr);
+  const set = new Set(arr);
+
+  set[Symbol.for('nodejs.util.inspect.custom')] = function() {
+    return `Set { ${Array.from(this).join(', ')} }`;
+  };
+
+  return set;
 }
 
