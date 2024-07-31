d465187007767b429de2f4763987594f80815528
Medo20300 <medhatconcrete@gmail.com>
2024-07-31 14:44:16 +0300

    Programming

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -2,9 +2,8 @@ export default function setFromArray(arr) {
   const set = new Set(arr);
 
   // Create a custom output for the Set object
-  set[Symbol.for('nodejs.util.inspect.custom')] = () => {
-    return `Set { ${Array.from(set).join(', ')} }`;
-  };
+  set[Symbol.for('nodejs.util.inspect.custom')] = () => `Set { ${Array.from(set).join(', ')} }`;
 
   return set;
 }
+
